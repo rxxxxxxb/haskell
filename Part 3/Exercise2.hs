d560d7dac45e54e920ab481b72208abd9fca1138
rakib <rxxxxxxb@gmail.com>
2019-03-08 01:15:54 +0100

    Added isElem

--- Part 3/Exercise2.hs
+++ Part 3/Exercise2.hs
@@ -10,6 +10,19 @@ insert cmp val (Branch l n r)
             | cmp val n == LT = Branch (insert cmp val l) n r
             | otherwise     = Branch l n (insert cmp val r)
 
+
+-- Check fo an element of an array
+isElem :: (a -> a -> Ordering) -> a -> SearchTree a -> Bool
+isElem cmp x Empty = False
+isElem cmp x (Branch l n r)
+            | cmp x n == EQ = True
+            | cmp x n == LT = (isElem cmp n l)
+            | otherwise     = (isElem cmp n r)
+
+
+
 tree :: SearchTree Int 
 -- tree = Branch (Empty) 10 (Empty)                   
 tree = Branch (Branch Empty 10 Empty) 20 (Branch Empty 30 Empty)
+
+
